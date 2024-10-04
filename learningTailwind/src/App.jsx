e2e1e126a33e22622aab1423cc17d7058f9426fd
sanket9673 <sanketch9673@gmail.com>
2024-10-04 23:46:15 +0530

    Demo Project on TailwindCss

---
+++ learningTailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css" ;
+
+const App = () => {
+  return (
+    <section className="m-12 flex bg-yellow-600">
+      <p>Welcome to my website made with html css js react and TAILWIND_CSS</p>
+    </section>
+  ) ;
+}
+
+export default App ;
